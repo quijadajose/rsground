905a6b2b81dae825d487ea6af03c6a95ae244968
Brayan-724 <bjqf0724@gmail.com>
2025-03-22 14:58:02 -0700

    feat(frontend/sidebar): better animation in nav buttons

--- frontend/src/core/sidebar/Sidebar.tsx
+++ frontend/src/core/sidebar/Sidebar.tsx
@@ -1,6 +1,6 @@
 import Accordion from "@corvu/accordion";
-import { createSignal, ParentProps } from "solid-js";
-import style from "./style.module.sass";
+import { ParentProps } from "solid-js";
+import style from "./Sidebar.module.sass";
 import { isSidebarOpen, setIsSidebarOpen } from "./store";
 
 function SidebarItem(props: ParentProps<{ title: string }>) {
@@ -35,6 +35,36 @@ export function Sidebar() {
             >
               <path
                 d="M4 5H16a1 1 0 010 2H4a1 1 0 010-2Zm0 4H16a1 1 0 010 2H4a1 1 0 010-2Zm0 4H16a1 1 0 010 2H4a1 1 0 010-2Z"
+                fill="currentColor"
+              />
+            </svg>
+          </li>
+
+          <li class={style.nav_item}>
+            <svg
+              width="20"
+              height="20"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M7 2H15L20 6V19C20 19.7956 19.6839 20.5587 19.1213 21.1213C18.5587 21.6839 17.7956 22 17 22H7C6.20435 22 5.44129 21.6839 4.87868 21.1213C4.31607 20.5587 4 19.7956 4 19V5C4 4.20435 4.31607 3.44129 4.87868 2.87868C5.44129 2.31607 6.20435 2 7 2Z"
+                fill="currentColor"
+              />
+            </svg>
+          </li>
+
+          <li class={style.nav_item}>
+            <svg
+              width="20"
+              height="20"
+              viewBox="0 0 24 24"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M7 2H15L20 6V19C20 19.7956 19.6839 20.5587 19.1213 21.1213C18.5587 21.6839 17.7956 22 17 22H7C6.20435 22 5.44129 21.6839 4.87868 21.1213C4.31607 20.5587 4 19.7956 4 19V5C4 4.20435 4.31607 3.44129 4.87868 2.87868C5.44129 2.31607 6.20435 2 7 2Z"
                 fill="currentColor"
               />
             </svg>
