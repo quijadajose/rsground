f433e96143647c58f662dedeac42ae30cebe589d
Brayan-724 <bjqf0724@gmail.com>
2025-03-30 09:29:46 -0700

    feat(feature/colab): Add horizontal layout

--- frontend/src/core/colab/Colab.tsx
+++ frontend/src/core/colab/Colab.tsx
@@ -13,7 +13,7 @@ import styles from "./Colab.module.sass";
 import { For } from "solid-js";
 
 export function Colab() {
-  const requestUsers = ["CHIWO", "Jopzgo", "gg0074x"];
+  const requestUsers = ["CHIWO", "Jopzgo", "gg0074x", "Otro"];
 
   return (
     <Dialog open={isColabOpen()} onOpenChange={setIsColabOpen}>
@@ -22,82 +22,97 @@ export function Colab() {
         <Dialog.Content class={styles.content}>
           <h2 class={styles.title}>Live Collaboration</h2>
 
-          <label class={styles.text_input}>
+          <div class={styles.container}>
             <div>
-              <FaSolidUser />
-              <input placeholder="Write your name" />
+              <h3 class={styles.subtitle}>Room settings</h3>
+              <label class={styles.text_input}>
+                <div>
+                  <FaSolidUser />
+                  <input placeholder="Write your name" />
+                </div>
+              </label>
+
+              <label class={styles.checkbox_input}>
+                Public room
+                <input type="checkbox" />
+              </label>
+
+              <label class={styles.text_input}>
+                <div>
+                  <FaSolidLock />
+                  <input placeholder="Leave empty for no password" />
+                </div>
+              </label>
             </div>
-          </label>
 
-          <label class={styles.checkbox_input}>
-            Public room
-            <input type="checkbox" />
-          </label>
-
-          <label class={styles.text_input}>
             <div>
-              <FaSolidLock />
-              <input placeholder="Leave empty for no password" />
+              <h3 class={styles.subtitle}>Members</h3>
+              <label class={styles.text_input}>
+                <div>
+                  <FaBrandsGithub />
+                  <input placeholder="Username" />
+                </div>
+              </label>
+              <ul class={styles.user_list}>
+                <For each={requestUsers}>
+                  {(name, idx) => (
+                    <li class={styles.member}>
+                      <span class={styles.member_name}>
+                        {name}
+                      </span>
+
+                      <Popover placement="right-start">
+                        <Popover.Trigger class={styles.select_box}>
+                          <span>{idx() % 2 === 0 ? "Editor" : "Viewer"}</span>
+                          <div>
+                            <FaSolidChevronDown width="0.5em" height="0.5em" />
+                          </div>
+                        </Popover.Trigger>
+                        <Popover.Portal>
+                          <Popover.Content as="ul" class={styles.select_list}>
+                            <label>
+                              <input
+                                type="radio"
+                                name="select"
+                                checked={idx() % 2 === 0}
+                              />
+                              <span>Editor</span>
+                            </label>
+                            <label>
+                              <input
+                                type="radio"
+                                name="select"
+                                checked={idx() % 2 !== 0}
+                              />
+                              <span>Viewer</span>
+                            </label>
+                          </Popover.Content>
+                        </Popover.Portal>
+                      </Popover>
+                    </li>
+                  )}
+                </For>
+              </ul>
+
+              <h3 class={styles.subtitle}>Pending Requests</h3>
+              <ul class={styles.user_list}>
+                <For each={requestUsers}>
+                  {(name) => (
+                    <li class={styles.member}>
+                      <span class={styles.member_name}>
+                        {name}
+                      </span>
+
+                      <ul class={styles.button_group}>
+                        <button class={styles.success}>Allow</button>
+                        <button class={styles.error}>Kick</button>
+                      </ul>
+                    </li>
+                  )}
+                </For>
+              </ul>
             </div>
-          </label>
-
-          <h3 class={styles.subtitle}>Members</h3>
-          <label class={styles.text_input}>
-            <div>
-              <FaBrandsGithub />
-              <input placeholder="Username" />
-            </div>
-          </label>
-          <ul class={styles.user_list}>
-            <For each={requestUsers}>
-              {(name, idx) => (
-                <li class={styles.member}>
-                  <span class={styles.member_name}>
-                    {name}
-                  </span>
-
-                  <Popover placement="right-start">
-                    <Popover.Trigger class={styles.select_box}>
-                      <span>{idx() % 2 === 0 ? "Editor" : "Viewer"}</span>
-                      <div>
-                        <FaSolidChevronDown width="0.5em" height="0.5em" />
-                      </div>
-                    </Popover.Trigger>
-                    <Popover.Portal>
-                      <Popover.Content as="ul" class={styles.select_list}>
-                        <label>
-                          <input type="radio" name="select" checked={idx() % 2 === 0} />
-                          <span>Editor</span>
-                        </label>
-                        <label>
-                          <input type="radio" name="select" checked={idx() % 2 !== 0} />
-                          <span>Viewer</span>
-                        </label>
-                      </Popover.Content>
-                    </Popover.Portal>
-                  </Popover>
-                </li>
-              )}
-            </For>
-          </ul>
-
-          <h3 class={styles.subtitle}>Pending Requests</h3>
-          <ul class={styles.user_list}>
-            <For each={requestUsers}>
-              {(name) => (
-                <li class={styles.member}>
-                  <span class={styles.member_name}>
-                    {name}
-                  </span>
-
-                  <ul class={styles.button_group}>
-                    <button class={styles.success}>Allow</button>
-                    <button class={styles.error}>Kick</button>
-                  </ul>
-                </li>
-              )}
-            </For>
-          </ul>
+          </div>
         </Dialog.Content>
       </Dialog.Portal>
     </Dialog>
