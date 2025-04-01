9e65ec74bc269caab2f12584cc0ab674130d83c3
Brayan-724 <bjqf0724@gmail.com>
2025-04-01 10:48:16 -0700

    feat(frontend/panels): Add completion to editor

--- frontend/src/core/panels/CodePanel.tsx
+++ frontend/src/core/panels/CodePanel.tsx
@@ -1,8 +1,9 @@
 import { createSignal } from "solid-js";
 import { CodeMirror } from "@solid-codemirror/codemirror";
 import { basicSetup } from "codemirror";
-import { rust } from "@codemirror/lang-rust";
+import { rust, rustLanguage } from "@codemirror/lang-rust";
 import { HighlightStyle, syntaxHighlighting } from "@codemirror/language";
+import { autocompletion, CompletionContext } from "@codemirror/autocomplete";
 import { tags as t } from "@lezer/highlight";
 
 import styles from "./CodePanel.module.sass";
@@ -24,16 +25,69 @@ const rsgroundTheme = HighlightStyle.define([
   { tag: t.special(t.variableName), class: styles.lifetime },
 ]);
 
+const keywords = [
+  "pub",
+  "fn",
+  "struct",
+  "let",
+];
+
+const localCompletions = [
+  ...(keywords.map((keyword) => ({
+    label: keyword,
+    type: "keyword",
+  }))),
+];
+
+const CODE_EXAMPLE = `
+pub struct Something {
+  prop: String
+}
+
+#[tokio::main]
+fn main<'a>() {
+  let a = String::new();
+  println!(r#"{a}"#);
+}`;
+
 export function CodePanel() {
-  const [code, setCode] = createSignal(
-    'pub struct Something {\n  prop: String\n}\n\n#[tokio::main]\nfn main<\'a>() {\n  let a = String::new();\n  println!(r#"{a}"#);\n}',
-  );
+  const [code, setCode] = createSignal(CODE_EXAMPLE);
 
   return (
     <CodeMirror
       class={styles.container}
       value={code()}
-      extensions={[basicSetup, syntaxHighlighting(rsgroundTheme), rust()]}
+      extensions={[
+        basicSetup,
+        syntaxHighlighting(rsgroundTheme),
+        autocompletion({
+          interactionDelay: 0,
+          activateOnTypingDelay: 0,
+          closeOnBlur: false,
+          tooltipClass: () => styles.completion_tooltip,
+          optionClass: (completion) => {
+            const typeClass = completion.type
+              ? " " + styles["completion_t_" + completion.type]
+              : "";
+
+            return styles.completion_option + typeClass;
+          },
+        }),
+        rust(),
+        rustLanguage.data.of({
+          "autocomplete": async (context: CompletionContext) => {
+            let word = context.matchBefore(/\w*/);
+            if (word.from == word.to && !context.explicit) {
+              return null;
+            }
+
+            return {
+              from: word.from,
+              options: localCompletions,
+            };
+          },
+        }),
+      ]}
     />
   );
 }
